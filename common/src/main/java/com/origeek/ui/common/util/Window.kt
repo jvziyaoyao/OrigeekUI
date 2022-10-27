f9edc83d188e38852e287345a82932bc62f41044
shijie.zhong <shijie.zhong@hiscene.com>
2022-10-27 18:26:06 +0800

    feat: 增加picker的utils方法

--- common/src/main/java/com/origeek/ui/common/util/Window.kt
+++ common/src/main/java/com/origeek/ui/common/util/Window.kt
@@ -1,5 +1,8 @@
 package com.origeek.ui.common.util
 
+import android.app.Activity
+import android.content.Context
+import android.content.ContextWrapper
 import android.view.Window
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
@@ -27,4 +30,13 @@ fun showSystemUI(window: Window) {
         window,
         window.decorView
     ).show(WindowInsetsCompat.Type.systemBars())
+}
+
+fun Context.findWindow(): Window? {
+    var context = this
+    while (context is ContextWrapper) {
+        if (context is Activity) return context.window
+        context = context.baseContext
+    }
+    return null
 }
